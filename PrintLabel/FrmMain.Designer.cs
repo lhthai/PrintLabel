e5ac83fca7095c3e89e0c49bcc8a3307187fe99e
Le Hong Thai <thai.le@action-composites.com.vn>
2020-07-15 15:59:53 +0700

    first commit

---
+++ PrintLabel/FrmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PrintLabel
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.dpDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbProduct = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dvData = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnTest.FlatAppearance.BorderSize = 0;
+            this.btnTest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.ForeColor = System.Drawing.Color.White;
+            this.btnTest.Location = new System.Drawing.Point(291, 189);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(91, 36);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Preview";
+            this.btnTest.UseVisualStyleBackColor = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLoad.FlatAppearance.BorderSize = 0;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.Color.White;
+            this.btnLoad.Location = new System.Drawing.Point(149, 189);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(91, 36);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPrint);
+            this.groupBox1.Controls.Add(this.dpDate);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbProduct);
+            this.groupBox1.Controls.Add(this.btnTest);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnLoad);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(27, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(617, 253);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose release date";
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnPrint.FlatAppearance.BorderSize = 0;
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.ForeColor = System.Drawing.Color.White;
+            this.btnPrint.Location = new System.Drawing.Point(435, 189);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(91, 36);
+            this.btnPrint.TabIndex = 6;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // dpDate
+            // 
+            this.dpDate.CustomFormat = "";
+            this.dpDate.Location = new System.Drawing.Point(149, 41);
+            this.dpDate.Name = "dpDate";
+            this.dpDate.Size = new System.Drawing.Size(233, 22);
+            this.dpDate.TabIndex = 5;
+            this.dpDate.ValueChanged += new System.EventHandler(this.dpDate_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date:";
+            // 
+            // cbProduct
+            // 
+            this.cbProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbProduct.FormattingEnabled = true;
+            this.cbProduct.Location = new System.Drawing.Point(149, 90);
+            this.cbProduct.Name = "cbProduct";
+            this.cbProduct.Size = new System.Drawing.Size(233, 24);
+            this.cbProduct.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Product:";
+            // 
+            // dvData
+            // 
+            this.dvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvData.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dvData.Location = new System.Drawing.Point(0, 291);
+            this.dvData.Name = "dvData";
+            this.dvData.Size = new System.Drawing.Size(973, 288);
+            this.dvData.TabIndex = 5;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(833, 263);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(52, 16);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Total: ";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 579);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dvData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Print Label";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dpDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbProduct;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dvData;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
---
+++ PrintLabel/FrmMain.cs
@@ -0,0 +1,85 @@
+﻿using PrintLabel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintLabel
+{
+    public partial class FrmMain : Form
+    {
+        string connectionString = "Data Source=10.50.4.4;Initial Catalog=VACP4DB;User ID=sa; Password=ACVN1234~!;";
+        LoadBatchcodeFromWO loadBatchcodeFromWO = new LoadBatchcodeFromWO();
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadProduct()
+        {
+            string queryString = $"SELECT DISTINCT(PRODUCT) FROM BATCHCODE WHERE CONVERT(DATE, CREATEDATE, 103) = '{dpDate.Value.ToString("MM/dd/yyyy")}'";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                try
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(queryString, connectionString);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        cbProduct.DataSource = dt;
+                        cbProduct.DisplayMember = "Product";
+                        cbProduct.ValueMember = "Product";
+                    }
+                    else
+                    {
+                        MessageBox.Show("No release WO this day!");                       
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void cbProductGroup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadProduct();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            DataRowView drv = (DataRowView)cbProduct.SelectedItem;
+            String product = drv["Product"].ToString().Trim();
+            DataTable dt = loadBatchcodeFromWO.LoadBatchcode(dpDate.Value.ToString("MM/dd/yyyy"), product);
+            dvData.DataSource = dt;
+            lblTotal.Text = $"Total: {dt.Rows.Count} labels";
+        }
+
+        private void dpDate_ValueChanged(object sender, EventArgs e)
+        {
+            LoadProduct();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            DataRowView drv = (DataRowView)cbProduct.SelectedItem;
+            String product = drv["Product"].ToString().Trim();
+            DataTable dt = loadBatchcodeFromWO.LoadBatchcode(dpDate.Value.ToString("MM/dd/yyyy"), product);
+            Printer printer = new Printer();
+            printer.PrintAll(product, dt);
+        }
+    }
+}
